14b2e4ca236eb642e2663c11852efacdbe598bf2
B4SH <b4sh@geebee.org>
2018-04-01 22:11:55 +0200

    script for updating your git repos for your whole hd

---
+++ 4misc/4maintenance/4git.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################################
+#
+# Idea / Written by Sebastian Vivian Gresser - All Rights Reserved
+#
+# Copyright (C) by geeBee UG (haftungsbeschränkt) - All Rights Reserved
+# Copyright (C) by Sebastian Vivian Gresser - All Rights Reserved
+#
+########################################################################
+
+
+import os, sys, re, subprocess
+
+try:
+    src_dirs=sys.argv[1]
+except:
+    src_dirs=os.getcwd()
+    print("no source dir specified | usage update_sources.py directory \nFalling back to cwd..."+src_dirs)
+
+
+for sdir in os.walk(src_dirs):    
+    if(os.path.isdir(sdir[0]+"/.git")):
+        sdir = sdir[0]
+        cmd=["screen", "-dmt", "git_repo_worker", "git", "-C", sdir, "pull"]
+        print(cmd)
+        subprocess.Popen(cmd)
+    else:
+        continue
+            
+            
+
+
+
+
